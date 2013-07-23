0a1a1cbd706e069edc2ddadc176af42db5c2d7c7
Alexey Bugakov <alexeybugakov@gmail.com>
2013-07-23 17:55:36 +0400

    just 3x3

--- src/com.tictactoe/Main.java
+++ src/com.tictactoe/Main.java
@@ -6,6 +6,7 @@ import com.tictactoe.io.IO;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("q");
         Game game = new Game();
         while (true) {
             game.playGame();
